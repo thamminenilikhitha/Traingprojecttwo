cc46ea93145ca5219655fb22719a315648162997
thamminenilikhitha <thamminenilikhitha@gmail.com>
2025-03-11 12:54:59 +0530

    project commited and Alogrithm

---
+++ src/math.java
@@ -0,0 +1,13 @@
+public class math {
+
+    public static void main(String[] args) {
+
+        int a=30;
+        int c=40;
+        System.out.println(a+c);
+        System.out.println(a-c);
+        System.out.println(a/c);
+        System.out.println(a*c);
+
+    }
+}
